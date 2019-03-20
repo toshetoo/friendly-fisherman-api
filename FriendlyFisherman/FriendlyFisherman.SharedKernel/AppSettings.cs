3d4d12686f50cb0e9c630d51c9244cd519043d70
Todor Ivanov <todor.ivanov@primeholding.com>
2019-03-20 09:48:08 +0200

    Add email settings to global settings

--- FriendlyFisherman/FriendlyFisherman.SharedKernel/AppSettings.cs
+++ FriendlyFisherman/FriendlyFisherman.SharedKernel/AppSettings.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FriendlyFisherman.SharedKernel.Settings;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace FriendlyFisherman.SharedKernel
 {
     public class AppSettings
     {
         public string Secret { get; set; }
+        public EmailSettings EmailSettings { get; set; }
     }
 }
