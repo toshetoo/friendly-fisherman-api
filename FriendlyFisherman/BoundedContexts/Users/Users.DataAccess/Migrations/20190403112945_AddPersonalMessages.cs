563b525f4fa03a681214ede9009e1020cb663377
Todor Ivanov <ivanov.todor1@gmail.com>
2019-04-03 14:30:30 +0300

    Add Migration for PersonalMessages DB Table

---
+++ FriendlyFisherman/BoundedContexts/Users/Users.DataAccess/Migrations/20190403112945_AddPersonalMessages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Users.DataAccess.Migrations
+{
+    public partial class AddPersonalMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPersonalMessages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    SenderId = table.Column<string>(nullable: true),
+                    ReceiverId = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Seen = table.Column<bool>(nullable: false),
+                    SentOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPersonalMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPersonalMessages");
+        }
+    }
+}
